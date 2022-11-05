bc0c4dbfb3fd52ff9a726a700dbcc43982e3c268
Kvothe Guile <112582839+KvotheGuile@users.noreply.github.com>
2022-11-04 20:29:31 -0700

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,8 +1,34 @@
 input.onButtonPressed(Button.A, function () {
     Player.change(LedSpriteProperty.X, -1)
 })
+function Over () {
+    gameOn = 0
+    score = Math.round(control.millis() / 5000)
+    music.startMelody(music.builtInMelody(Melodies.Dadadadum), MelodyOptions.ForeverInBackground)
+    Player.delete()
+    for (let index = 0; index <= 4; index++) {
+        list[index].delete()
+    }
+    while (true) {
+        for (let index = 0; index < 3; index++) {
+            basic.showLeds(`
+                # . . . #
+                . # . # .
+                . . # . .
+                . # . # .
+                # . . . #
+                `)
+            basic.showNumber(score)
+        }
+        basic.showString("RESTART>LOGO")
+        basic.pause(1000)
+    }
+}
 input.onButtonPressed(Button.B, function () {
     Player.change(LedSpriteProperty.X, 1)
+})
+input.onLogoEvent(TouchButtonEvent.Pressed, function () {
+    control.reset()
 })
 let list: game.LedSprite[] = []
 let Meteor4 = 0
@@ -11,7 +37,11 @@ let Meteor2 = 0
 let Meteor1 = 0
 let Meteor0 = 0
 let Player: game.LedSprite = null
-let speed = 500
+let gameOn = 0
+let score = 0
+score = 0
+gameOn = 1
+let speed = 600
 Player = game.createSprite(2, 4)
 let meteors = [
 Meteor0,
@@ -22,15 +52,30 @@ Meteor4
 ]
 for (let index = 0; index <= 4; index++) {
     list[index] = game.createSprite(index, 0)
-    list[index].set(LedSpriteProperty.Brightness, 20)
+    list[index].set(LedSpriteProperty.Brightness, 5)
 }
 basic.forever(function () {
     basic.pause(speed)
+    if (gameOn == 1) {
+        for (let index = 0; index <= 4; index++) {
+            if (list[index].get(LedSpriteProperty.Y) == 4) {
+                list[index].set(LedSpriteProperty.Y, 0)
+                list[index].set(LedSpriteProperty.Brightness, 5)
+            }
+            if (list[index].get(LedSpriteProperty.Y) < 4) {
+                if (list[index].get(LedSpriteProperty.Brightness) >= 20) {
+                    list[index].change(LedSpriteProperty.Y, 1)
+                } else if (0 == randint(0, 1)) {
+                    list[index].set(LedSpriteProperty.Brightness, 110)
+                }
+            }
+        }
+    }
+})
+loops.everyInterval(speed / 2, function () {
     for (let index = 0; index <= 4; index++) {
-        if (list[index].get(LedSpriteProperty.Y) < 4) {
-            list[index].change(LedSpriteProperty.Y, 1)
-        } else {
-            list[index].set(LedSpriteProperty.Y, 0)
+        if (Player.isTouching(list[index])) {
+            Over()
         }
     }
 })
